a10211c33bf4bf1045a61082221657a6652f5f49
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-06-26 19:54:53 +0300

    2 Done function "print_arr" in order to display the result in console

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fillArr(size):
+def fill_arr(size):
     arr = []
     for i in range(size):
         temp = []
@@ -12,9 +12,34 @@ def fillArr(size):
                 else:
                     temp.append(" ")
         arr.append(temp)
-        return arr
 
+    return arr
+
+def print_arr(arr):
+    for i in range(sizeA):
+        if i == 0:
+            print(" ", end='\t')
+        elif i == sizeA - 1:
+            print("(Y)", end='\t')
+        else:
+            print((sizeA - 1) - i, end='\t')
+        for j in range(sizeA):
+            print(arr[i][j], end='  ')
+        print()
+        if i == sizeA - 1:
+            print("   ", end='')
+            for n in range(sizeA - 1):
+                if n == 0:
+                    print("(X) ", end='')
+                else:
+                    print(n, end='  ')
+            print()
+
+#--------------------------------------------------------#
 sizeA: int = 10
-fillArr(sizeA)
+
 print("\tStart\n\t\tGame \"Serpentine\"")
 print("\t\tmatrix size {} x {}".format(sizeA, sizeA))
+
+arr_game = fill_arr(sizeA)
+print_arr(arr_game)

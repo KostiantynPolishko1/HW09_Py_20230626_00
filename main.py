eb981c6b8e5d211c37aaeaebe82920ac91a9bce7
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-06-26 19:20:01 +0300

    1 Done function "fillArr in order to fill field of game

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Start point")
+def fillArr(size):
+    arr = []
+    for i in range(size):
+        temp = []
+        if i == 0 or i == size - 1:
+            for j in range(size):
+                temp.append("*")
+        else:
+            for j in range(size):
+                if j == 0 or j == size - 1:
+                    temp.append("*")
+                else:
+                    temp.append(" ")
+        arr.append(temp)
+        return arr
+
+sizeA: int = 10
+fillArr(sizeA)
+print("\tStart\n\t\tGame \"Serpentine\"")
+print("\t\tmatrix size {} x {}".format(sizeA, sizeA))
